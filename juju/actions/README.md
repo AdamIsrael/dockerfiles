3af76e72beb75fcfcef6829eefa9a7cc24e71b19
Adam Israel <adam@adamisrael.com>
2015-03-15 18:19:59 -0400

    WIP - add LXC support

--- juju/actions/README.md
+++ juju/actions/README.md
@@ -1,1 +1,10 @@
-## Juju Actions
+# Juju Actions
+
+The goal of this docker image is to give users a way of using the new `actions` feature of Juju before it's available for general release.
+
+## Building
+docker build jujubox ./
+
+
+## TODO
+- Create Makefile to build, test, and upload image
--- juju/actions/juju.sh
+++ juju/actions/juju.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 set -e
 
+
+apt-get -qy install juju-core juju-quickstart juju-deployer juju-local
+
 # Fetch latest code
-go get launchpad.net/godeps/...
-go get -v github.com/juju/juju/...
+# go get launchpad.net/godeps/...
+# go get -v github.com/juju/juju/...
 
 # Build!
-cd ~/go/src/github.com/juju/juju
-JUJU_MAKE_GODEPS=true make godeps
-make build
-$GOPATH/bin/juju version
+# cd ~/go/src/github.com/juju/juju
+# JUJU_MAKE_GODEPS=true make godeps
+# make build
+# $GOPATH/bin/juju version
