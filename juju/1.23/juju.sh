7ae3afa3892c0bec474c476e05909ed1e1894c60
Adam Israel <adam@adamisrael.com>
2015-03-27 11:35:20 -0600

    Switch to PPA, and fix environments.yaml

--- juju/1.23/juju.sh
+++ juju/1.23/juju.sh
@@ -8,28 +8,12 @@ RC=${HOME}/.bashrc
 echo "export JUJU_HOME=${HOME}/.juju" >> $RC
 echo "export JUJU_REPOSITORY=${HOME}" >> $RC
 
-mkdir -p $HOME/.go
-export GOPATH=$HOME/.go
-export GOROOT=/usr/lib/go
+echo -e "deb http://ppa.launchpad.net/juju/devel/ubuntu trusty main\n \
+deb-src http://ppa.launchpad.net/juju/devel/ubuntu trusty main" >> /etc/apt/sources.list.d/juju-devel.list
 
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 376A290EC8068B11
+
+apt-get update
 apt-get -qy install juju-core juju-quickstart juju-deployer
 
-# Fetch latest code
-go get launchpad.net/godeps/...
-# mkdir -p $HOME/.go/src/github.com/juju/juju
-#
-# wget https://github.com/juju/juju/archive/juju-1.23-beta2.tar.gz
-# mkdir -p .go/src/github.com/juju/juju
-# tar xf juju-1.23-beta2.tar.gz -C .go/src/github.com/juju/juju/ --strip-components=1
-# rm juju-1.23-beta2.tar.gz
-#
-git clone https://github.com/juju/juju $HOME/.go/src/github.com/juju/juju
-cd $HOME/.go/src/github.com/juju/juju
-git checkout 1.23
-
-# Build!
-JUJU_MAKE_GODEPS=true make godeps
-make build
-make install
-
-echo "export PATH=${HOME}/.go/bin:$PATH:${HOME}/.juju-plugins" >> $RC
+echo "export PATH=$PATH:${HOME}/.juju-plugins" >> $RC
